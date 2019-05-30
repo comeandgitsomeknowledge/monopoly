29986ff5e90320e87e49f45f1c2c9236091c7685
Hannah Vaughan <vaughanh@oregonstate.edu>
2019-05-30 13:05:34 -0700

    fixed unit tests

--- test/collect_each_player_test.js
+++ test/collect_each_player_test.js
@@ -11,7 +11,8 @@ describe('Collect from Each Player Tests', function () {
         collectfromeachplayer(50, "Testing Fine");
         updateMoney();
 
-        assert.deepEqual(player[2].money, 1450, "Player 2 ends with $1450");
+        assert.deepEqual(player[1].money, 650, "Player 1 ends with $1450");
+        assert.deepEqual(player[2].money, 1600, "Player 2 ends with $1600");
         assert.deepEqual(player[3].money, 1450, "Player 3 ends with $1450");
     });
 
@@ -25,33 +26,33 @@ describe('Collect from Each Player Tests', function () {
     });
 
     it('removes exactly remaining money from players', function () {
-        assert.deepEqual(player[2].money, 1400, "Player 2 starts with $1400");
+        assert.deepEqual(player[1].money, 600, "Player 1 starts with $600");
         assert.deepEqual(player[3].money, 1400, "Player 3 starts with $1400");
 
         collectfromeachplayer(1400, "Broooooke");
         updateMoney();
 
-        assert.deepEqual(player[2].money, 0, "Player 2 has no more money");
+        assert.deepEqual(player[1].money, 0, "Player 1 has no more money");
         assert.deepEqual(player[3].money, 0, "Player 3 has no more money");
     });
 
     it('collects exact amount of money from all players', function () {
-        assert.deepEqual(player[turn].money, 4500, "Current player starts with $4500");
+        assert.deepEqual(player[turn].money, 3700, "Current player starts with $3700");
 
         collectfromeachplayer(1500, "Bork");
         updateMoney();
 
-        assert.deepEqual(player[turn].money, 4500, "Current player ends with $4500");
+        assert.deepEqual(player[turn].money, 3700, "Current player ends with $3700");
     });
 
     it('requests more money than players have but collects total money', function () {
-        assert.deepEqual(player[2].money, 0, "Player 2 starts with no money");
+        assert.deepEqual(player[1].money, 0, "Player 1 starts with no money");
         assert.deepEqual(player[3].money, 0, "Player 3 starts with no money");
         
         collectfromeachplayer(2000, "Eeeeextraaaaa broooooke");
         updateMoney();
 
-        assert.deepEqual(player[2].money, 0, "Player 2 ends with no money");
+        assert.deepEqual(player[1].money, 0, "Player 1 ends with no money");
         assert.deepEqual(player[3].money, 0, "Player 3 ends with no money");
     });
 });
